7cd57cacfdd83ce17928a8841209a6d355ffd613
ermvrs <ermvrs@live.com>
2025-01-17 20:47:31 +0300

    wagmi init

--- src/index.js
+++ src/index.js
@@ -3,14 +3,43 @@ import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { http, createConfig, WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+const rosettanetSepolia = {
+  id: 1381192787,
+  name: 'Rosettanet',
+  nativeCurrency: { name: 'Starknet Token', symbol: 'STRK', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['http://localhost:3000'] },
+  },
+  blockExplorers: {
+    default: { name: 'Voyager', url: 'https://sepolia.voyager.io' },
+  },
+}
+
+export const config = createConfig({
+  chains: [rosettanetSepolia],
+  transports: {
+    [rosettanetSepolia]: http(),
+  },
+})
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
+const queryClient = new QueryClient()
+
 root.render(
   <StrictMode>
+  <WagmiProvider  config={config}>
+  <QueryClientProvider client={queryClient}> 
     <ColorModeScript />
     <App />
+    </QueryClientProvider>  
+  </WagmiProvider>
+
   </StrictMode>
 );
 
--- src/views/starkgate/Starkgate.jsx
+++ src/views/starkgate/Starkgate.jsx
@@ -1,5 +1,33 @@
+import { useAccount, useBalance, useConnect } from "wagmi"
+import { Box, Button, Container, Flex, Text } from "@chakra-ui/react"
+
 export default function Starkgate() {
+    const { address } = useAccount()
+    const balance = useBalance({
+        address
+    })
     return (
-        <>Starkgate</>
+        <Container maxW='3xl'>
+            <Flex flexDirection={'row'} alignItems={'center'}>
+                <Flex alignItems={'center'} flexDirection={'column'}>
+                    <Text>Wallet Options</Text>
+                    <WalletOptions /> 
+                </Flex>
+                <Flex>
+                    <Text>Connected to: {address}</Text>
+                    <Text>Balance: {balance} STRK</Text>
+                </Flex>
+            </Flex>
+        </Container>
     )
 }
+
+export function WalletOptions() {
+    const { connectors, connect } = useConnect()
+  
+    return connectors.map((connector) => (
+      <Button key={connector.uid} onClick={() => connect({ connector })}>
+        {connector.name}
+      </Button>
+    ))
+  }
